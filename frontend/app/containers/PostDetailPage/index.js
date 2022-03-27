222e50e9df48e38b32e54848f0780173b1f51aff
zhpeng811 <zhpeng@ualberta.ca>
2022-03-26 21:29:47 -0600

    FR5 - remove post button and backend logic

--- frontend/app/containers/PostDetailPage/index.js
+++ frontend/app/containers/PostDetailPage/index.js
@@ -1,6 +1,7 @@
 // react librairies
 import React from 'react';
 import { Helmet } from 'react-helmet';
+import { Redirect } from 'react-router-dom';
 import axios from 'axios';
 import { FormattedMessage } from 'react-intl';
 import moment from 'moment';
@@ -11,14 +12,21 @@ import Chip from '@mui/material/Chip';
 import TextField from '@mui/material/TextField';
 import ImageList from '@mui/material/ImageList';
 import ImageListItem from '@mui/material/ImageListItem';
-import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
 import Paper from '@mui/material/Paper';
 import Grid from '@mui/material/Grid';
+import Dialog from '@mui/material/Dialog';
+import DialogTitle from '@mui/material/DialogTitle';
+import DialogContent from '@mui/material/DialogContent';
+import DialogContentText from '@mui/material/DialogContentText';
+import DialogActions from '@mui/material/DialogActions';
 import InputAdornment from '@mui/material/InputAdornment';
+import IconButton from '@mui/material/IconButton';
 import ThumbUpIcon from '@mui/icons-material/ThumbUp';
 import CommentIcon from '@mui/icons-material/Comment';
 import SendIcon from '@mui/icons-material/Send';
 import FaceIcon from '@mui/icons-material/Face';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 // custom librairies and definitions
 import messages from './messages';
@@ -34,11 +42,15 @@ class PostDetailPage extends React.Component {
       post: null,
       postId: props.location.pathname.split('/')[2],
       liked: false,
-      comment: ""
+      comment: "",
+      deleteDialogState: "inactive",
+      // TODO: change to check with user
+      isAdmin: true,
     }
     
     this.onLikeButtonClick = this.onLikeButtonClick.bind(this);
     this.onCommentSend = this.onCommentSend.bind(this);
+    this.handleDelete = this.handleDelete.bind(this);
   }
 
   onImageClick = (image) => {
@@ -84,6 +96,22 @@ class PostDetailPage extends React.Component {
       });
   }
 
+  handleDelete = () => {
+    axios.delete(`${BACKEND_URL}/posts/${this.state.postId}/`)
+      .then(res => {
+        if (res.status === 204) {
+          this.setState({
+            deleteDialogState: "completed"
+          });
+        } else {
+          console.log("Failed to delete post: ", res);
+        }
+      })
+      .catch(err => {
+        console.log("Failed to delete post: ", err);
+      });
+  }
+
   componentDidMount() {
     axios.get(`${BACKEND_URL}/posts/${this.state.postId}`)
       .then(res => {
@@ -103,222 +131,269 @@ class PostDetailPage extends React.Component {
   render() {
     if (this.state.post === null) {
       return <LoadingIndicator />
-    }
+    } else if (this.state.deleteDialogState === "completed") {
+      return <Redirect to={{
+        pathname: '/',
+        state: { status: 'deletePostSuccess' }
+      }} />
+    } 
 
-      return (
-        <div>
-          <Helmet>
-            <title>Post Detail Page </title>
-          </Helmet>
-          
-          <Stack
-            component="form"
-            sx={{
-              width: '100%'
-            }}
-            spacing={2}
-            noValidate
-            autoComplete="off"
-          >
-            <H1>
-              <FormattedMessage {...messages.images} />
-            </H1>
+    return (
+      <div>
+        <Helmet>
+          <title>Post Detail Page </title>
+        </Helmet>
 
-            <ImageList variant="masonry" cols={3}>
-              {this.state.post.images.map((item) => (
-                  <ImageListItem 
-                      key={item}
-                      onClick={() => this.onImageClick(item)}
-                  >
-                  <img
-                      src={`${BACKEND_URL + item}?w=248&fit=crop&auto=format`}
-                      loading="lazy"
-                  />
-                  </ImageListItem>
-              ))}
-            </ImageList>
-            
-            <H2>
-              <FormattedMessage {...messages.tags} />
-            </H2>
-
-            <Stack 
-              direction={"row"} 
-              spacing={2}
-            >
-              {this.state.post.tags.split(",").map((tag) => (
-                <Chip key={tag} color="primary" label={tag} />
-              ))}
-            </Stack>
-            <H1>
-              <FormattedMessage {...messages.detail} />
-            </H1>
-
-            <TextField
-              id="title"
-              type="text"
-              label="Title"
-              variant="standard"
-              InputProps={{
-                  readOnly: true,
-              }}
-              value={this.state.post.title}
-            />
-
-            <TextField
-              id="author"
-              type="text"
-              label="Author"
-              variant="standard"
-              InputProps={{
-                readOnly: true,
-              }}
-              value={this.state.post.display_name}
-            />  
-
-            <TextField
-              multiline
-              fullWidth
-              id="description"
-              type="text"
-              label="Description"
-              variant="standard"
-              InputProps={{
-                readOnly: true,
-              }}
-              value={this.state.post.description}
-            />  
-
-            <Stack 
-              direction={"row"} 
-              spacing={2}
-            >
-              <TextField
-                id="created_date"
-                type="text"
-                label="Created Date"
-                variant="standard"
-                InputProps={{
-                  readOnly: true,
-                }}
-                sx={{
-                  width: '50%'
-                }}
-                value={moment(this.state.post.date_posted).format('YYYY-MM-DD HH:mm:ss')}
-              />
-
-              <TextField
-                id="Expiry Date"
-                type="text"
-                label="Expiry Date"
-                variant="standard"
-                InputProps={{
-                  readOnly: true,
-                }}
-                sx={{
-                  width: '50%'
-                }}
-                value={moment(this.state.post.date_expiry).format('YYYY-MM-DD HH:mm:ss')}
-              />
-
-            </Stack>
-            
-            <Stack 
+        <Stack
+          component="form"
+          sx={{
+            width: '100%'
+          }}
+          spacing={2}
+          noValidate
+          autoComplete="off"
+        >
+          {this.state.isAdmin && 
+            <Stack
               sx={{
-                position: 'relative',
-                alignItems: 'right',
-                justifyContent: 'right',
+                'position': 'relative',
+                'alignItems': 'center',
+                'justifyContent': 'center',
               }} 
               direction={"row"} 
             >
-              <TextField 
-                id="num_likes" 
-                variant="standard"
-                sx={{
-                  mt: '50px',
-                  width: '10%'
-                }} 
-                InputProps={{
-                  startAdornment: (
-                    <InputAdornment position="start">
-                      <ThumbUpIcon 
-                        color= {this.state.liked ? "success" : "action"} 
-                        fontSize="large"
-                        onClick={() => { this.onLikeButtonClick() }}
-                      />
-                    </InputAdornment>
-                  ),
-                  disableUnderline: true
-                }}
-                value={this.state.post.likes}
-              />
-
-              <TextField 
-                id="num_comments" 
-                variant="standard"
-                sx={{
-                  mt: '50px',
-                  width: '8%'
-                }} 
-                InputProps={{
-                  startAdornment: (
-                    <InputAdornment position="start">
-                      <CommentIcon
-                        color="primary" 
-                        fontSize="large"
-                      />
-                    </InputAdornment>
-                  ),
-                  disableUnderline: true
-                }}
-                value={this.state.post.comments.length}
-              />
+              <Button 
+                // sx={{"text-transform": "none"}}
+                variant='contained' 
+                color='error' 
+                startIcon={<DeleteIcon />}
+                onClick={() => this.setState({deleteDialogOpen: "active"})}
+              >
+                <FormattedMessage {...messages.delete} />
+              </Button>
+              <Dialog
+                open={this.state.deleteDialogOpen == "active"}
+                aria-labelledby="delete-dialog-title"
+                aria-describedby="delete-dialog-description"
+              >
+                <DialogTitle id="delete-dialog-title">
+                  <FormattedMessage {...messages.deleteDialogTitle} />
+                </DialogTitle>
+                <DialogContent>
+                  <DialogContentText id="delete-dialog-description">
+                    <FormattedMessage {...messages.deleteDialogDescription} />
+                  </DialogContentText>
+                </DialogContent>
+                <DialogActions>
+                  <Button onClick={() => this.setState({deleteDialogOpen: false})}>
+                    <FormattedMessage {...messages.cancel} />
+                  </Button>
+                  <Button autoFocus onClick={() => this.handleDelete()}>
+                    <FormattedMessage {...messages.confirm} />
+                  </Button>
+                </DialogActions>
+              </Dialog>
             </Stack>
+          }
 
-            <TextField 
-              id="add_comment" 
-              variant="outlined"
-              label="Add Comment"
-              placeholder="Add your comment here"
-              InputProps={{
-                endAdornment: (
-                  <InputAdornment position="end">
-                      <SendIcon
-                        color="primary"
-                        onClick={() => { this.onCommentSend() }}
-                      />
-                  </InputAdornment>
-                )
-              }}
-              value={this.state.comment}
-              onChange={(event) => this.setState({comment: event.target.value})}
-            />
+          <H1>
+            <FormattedMessage {...messages.images} />
+          </H1>
 
-            {/* https://codesandbox.io/s/comment-box-with-material-ui-10p3c */}
-            {this.state.post.comments.map((comment) => (
-              <Paper style={{ padding: "30px 20px" }} elavation={6}>
-                <Grid container wrap="nowrap" spacing={2}>
-                  <Grid item >
-                    <FaceIcon 
-                      color="primary"
-                      fontSize="large" 
-                    />
-                  </Grid>
-                  <Grid justifyContent="left" item xs zeroMinWidth>
-                    <h4 style={{ margin: 0, textAlign: "left" }}>{comment.username}</h4>
-                    <p style={{ textAlign: "left" }}>
-                      {comment.content}
-                    </p>
-                    <p style={{ textAlign: "left", color: "gray" }}>
-                      {"posted " + moment(comment.date).fromNow()}
-                    </p>
-                  </Grid>
-                </Grid>
-              </Paper>
+          <ImageList variant="masonry" cols={3}>
+            {this.state.post.images.map((item) => (
+                <ImageListItem 
+                    key={item}
+                    onClick={() => this.onImageClick(item)}
+                >
+                <img
+                    src={`${BACKEND_URL + item}?w=248&fit=crop&auto=format`}
+                    loading="lazy"
+                />
+                </ImageListItem>
+            ))}
+          </ImageList>
+          
+          <H2>
+            <FormattedMessage {...messages.tags} />
+          </H2>
+
+          <Stack 
+            direction={"row"} 
+            spacing={2}
+          >
+            {this.state.post.tags.split(",").map((tag) => (
+              <Chip key={tag} color="primary" label={tag} />
             ))}
           </Stack>
-        </div>
-      );
+          <H1>
+            <FormattedMessage {...messages.detail} />
+          </H1>
+
+          <TextField
+            id="title"
+            type="text"
+            label="Title"
+            variant="standard"
+            InputProps={{
+                readOnly: true,
+            }}
+            value={this.state.post.title}
+          />
+
+          <TextField
+            id="author"
+            type="text"
+            label="Author"
+            variant="standard"
+            InputProps={{
+              readOnly: true,
+            }}
+            value={this.state.post.display_name}
+          />  
+
+          <TextField
+            multiline
+            fullWidth
+            id="description"
+            type="text"
+            label="Description"
+            variant="standard"
+            InputProps={{
+              readOnly: true,
+            }}
+            value={this.state.post.description}
+          />  
+
+          <Stack 
+            direction={"row"} 
+            spacing={2}
+          >
+            <TextField
+              id="created_date"
+              type="text"
+              label="Created Date"
+              variant="standard"
+              InputProps={{
+                readOnly: true,
+              }}
+              sx={{
+                width: '50%'
+              }}
+              value={moment(this.state.post.date_posted).format('YYYY-MM-DD HH:mm:ss')}
+            />
+
+            <TextField
+              id="Expiry Date"
+              type="text"
+              label="Expiry Date"
+              variant="standard"
+              InputProps={{
+                readOnly: true,
+              }}
+              sx={{
+                width: '50%'
+              }}
+              value={moment(this.state.post.date_expiry).format('YYYY-MM-DD HH:mm:ss')}
+            />
+
+          </Stack>
+          
+          <Stack 
+            sx={{
+              position: 'relative',
+              alignItems: 'right',
+              justifyContent: 'right',
+            }} 
+            direction={"row"} 
+          >
+            <TextField 
+              id="num_likes" 
+              variant="standard"
+              sx={{
+                mt: '50px',
+                width: '10%' 
+              }} 
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <ThumbUpIcon 
+                      color= {this.state.liked ? "success" : "action"} 
+                      fontSize="large"
+                      onClick={() => { this.onLikeButtonClick() }}
+                    />
+                  </InputAdornment>
+                ),
+                disableUnderline: true
+              }}
+              value={this.state.post.likes}
+            />
+
+            <TextField 
+              id="num_comments" 
+              variant="standard"
+              sx={{
+                mt: '50px',
+                width: '8%'
+              }} 
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">
+                    <CommentIcon
+                      color="primary" 
+                      fontSize="large"
+                    />
+                  </InputAdornment>
+                ),
+                disableUnderline: true
+              }}
+              value={this.state.post.comments.length}
+            />
+          </Stack>
+
+          <TextField 
+            id="add_comment" 
+            variant="outlined"
+            label="Add Comment"
+            placeholder="Add your comment here"
+            InputProps={{
+              endAdornment: (
+                <InputAdornment position="end">
+                    <IconButton onClick={() => this.onCommentSend()}>
+                      <SendIcon color="primary" />
+                    </IconButton>
+                </InputAdornment>
+              )
+            }}
+            value={this.state.comment}
+            onChange={(event) => this.setState({comment: event.target.value})}
+          />
+
+          {/* https://codesandbox.io/s/comment-box-with-material-ui-10p3c */}
+          {this.state.post.comments.map((comment) => (
+            <Paper style={{ padding: "30px 20px" }} elavation={6}>
+              <Grid container wrap="nowrap" spacing={2}>
+                <Grid item >
+                  <FaceIcon 
+                    color="primary"
+                    fontSize="large" 
+                  />
+                </Grid>
+                <Grid justifyContent="left" item xs zeroMinWidth>
+                  <h4 style={{ margin: 0, textAlign: "left" }}>{comment.username}</h4>
+                  <p style={{ textAlign: "left" }}>
+                    {comment.content}
+                  </p>
+                  <p style={{ textAlign: "left", color: "gray" }}>
+                    {"posted " + moment(comment.date).fromNow()}
+                  </p>
+                </Grid>
+              </Grid>
+            </Paper>
+          ))}
+        </Stack>
+      </div>
+    );
   }
 }
 
--- frontend/app/containers/PostDetailPage/messages.js
+++ frontend/app/containers/PostDetailPage/messages.js
@@ -14,5 +14,25 @@ export default defineMessages({
   tags: {
     id: `${scope}.tags`,
     defaultMessage: 'Image Tags',
+  },
+  delete: {
+    id: `${scope}.delete`,
+    defaultMessage: 'Delete This Post',
+  },
+  deleteDialogTitle: {
+    id: `${scope}.deleteDialogTitle`,
+    defaultMessage: 'Are you sure you want to delete this post?',
+  },
+  deleteDialogDescription: {
+    id: `${scope}.deleteDialogDescription`,
+    defaultMessage: 'This will remove all images, tags, likes, and comments associated with this post. This action cannot be undone.',
+  },
+  cancel: {
+    id: `${scope}.cancel`,
+    defaultMessage: 'Cancel',
+  },
+  confirm: {
+    id: `${scope}.confirm`,
+    defaultMessage: 'Confirm',
   }
 });
